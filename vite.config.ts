bfe9341970f1ee22d4da17eed8daae515ed52a39
subhadeep.chowdhury <subhadeep.chowdhury@webileapps.com>
2025-03-09 00:13:46 +0530

    ci/cd fix

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,20 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: './',
+  build: {
+    outDir: 'dist',
+    sourcemap: true,
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          'vendor': ['react', 'react-dom', 'react-router-dom'],
+          'ag-grid': ['ag-charts-react', 'ag-charts-community'],
+        }
+      }
+    }
+  }
 })
